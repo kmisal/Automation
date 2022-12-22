2105049bb779df19c2aea4eff5c02c480d49c540
velocityrepository1234 <velocityrepository@gmail.com>
2022-12-22 09:53:52 +0530

    review comments adopted

--- src/main/java/pomClasses/InsuranceDahsboard.java
+++ src/main/java/pomClasses/InsuranceDahsboard.java
@@ -2,12 +2,11 @@ package pomClasses;
 
 public class KiteLoginPage {
 	
-	
 	@findBy (xpath ="//a") private WebElement fName
-	@findBy (xpath ="//a") private WebElement fName
-	@findBy (xpath ="//a") private WebElement fName
-	@findBy (xpath ="//a") private WebElement fName
-	@findBy (xpath ="//a") private WebElement fName
+	@findBy (xpath ="//a") private WebElement lName
+	@findBy (xpath ="//a") private WebElement mName
+	@findBy (xpath ="//a") private WebElement mob
+	@findBy (xpath ="//a") private WebElement email
 	
 	KiteLoginPage() {
 		
--- src/main/java/pomClasses/InsuranceLoginPage.java
+++ src/main/java/pomClasses/InsuranceLoginPage.java
@@ -4,12 +4,10 @@ public class KiteLoginPage {
 	
 	
 	@findBy (xpath ="//a") private WebElement fName
-	@findBy (xpath ="//a") private WebElement fName
-	@findBy (xpath ="//a") private WebElement fName
-	@findBy (xpath ="//a") private WebElement fName
-	
-	@findBy (xpath ="//a") private WebElement fName
-	@findBy (xpath ="//a") private WebElement fName
-	
+	@findBy (xpath ="//a") private WebElement lName
+	@findBy (xpath ="//a") private WebElement sName
+	@findBy (xpath ="//a") private WebElement mob	
+	@findBy (xpath ="//a") private WebElement email
+	@findBy (xpath ="//a") private WebElement dob	
 
 }
