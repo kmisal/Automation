29ed2da4398b60672def37d8333488a3d693dd1d
Kalyan Misal <kmisal32@gmail.com>
2022-12-22 21:11:21 +0530

    screenshot method added in utility class

--- src/main/java/baseclasses/utilityclass.java
+++ src/main/java/baseclasses/utilityclass.java
@@ -1,5 +1,18 @@
 package baseclasses;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
 public class utilityclass {
 
+	public static void screenshot(WebDriver driver) throws IOException {
+		File f1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File f2 = new File("C:\\Users\\user\\Desktop\\class notes\\GitHub\\Automation\\target\\screenshot\\image1.jpg");
+		FileHandler.copy(f1, f2);
+	}
 }
